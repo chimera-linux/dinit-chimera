6fe43cfacb308faac0b0b0c0d1845d9c7595f613
Daniel Kolesa <daniel@octaforge.org>
2022-11-20 17:10:56 +0100

    propagate container status to shutdown

--- early-scripts/tmpfs.sh
+++ early-scripts/tmpfs.sh
@@ -4,3 +4,7 @@ set -e
 
 mountpoint -q /run || mount -o mode=0755,nosuid,nodev -t tmpfs run /run
 mkdir -p -m0755 /run/lvm /run/user /run/lock /run/log
+
+if [ -n "${container+x}" ]; then
+    touch /run/system_is_container
+fi
