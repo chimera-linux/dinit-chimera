6b1dd50d419f47a63db8e96d6b506c8fc3f47649
Daniel Kolesa <daniel@octaforge.org>
2023-03-26 19:39:45 +0200

    pseudofs: only mount securityfs when dir exists

--- scripts/pseudofs.sh
+++ scripts/pseudofs.sh
@@ -10,7 +10,10 @@ mkdir -p -m0755 /dev/pts /dev/shm
 
 mountpoint -q /dev/pts || mount -o mode=0620,gid=5,nosuid,noexec -n -t devpts devpts /dev/pts
 mountpoint -q /dev/shm || mount -o mode=1777,nosuid,nodev -n -t tmpfs shm /dev/shm
-mountpoint -q /sys/kernel/security || mount -n -t securityfs securityfs /sys/kernel/security
+
+if [ -d /sys/kernel/security ]; then
+    mountpoint -q /sys/kernel/security || mount -n -t securityfs securityfs /sys/kernel/security
+fi
 
 if [ -d /sys/firmware/efi/efivars ]; then
     mountpoint -q /sys/firmware/efi/efivars || mount -o nosuid,noexec,nodev -t efivarfs efivarfs /sys/firmware/efi/efivars
